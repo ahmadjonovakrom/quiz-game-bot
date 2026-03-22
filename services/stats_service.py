03df6a6eb5178dd6c0f05ef80203bb9fa6b6db12
Ahmadjonov <dasturchi.tex@gmail.com>
2026-03-22 19:31:29 +0500

    mm

--- services/stats_service.py
+++ services/stats_service.py
@@ -1,5 +1,6 @@
 from database import (
     get_total_users_count,
+    get_total_players,
     get_total_questions_count,
     get_total_games,
     get_total_groups,
@@ -9,6 +10,7 @@ from database import (
 def get_bot_stats_service():
     return {
         "total_users": get_total_users_count(),
+        "total_players": get_total_players(),
         "total_questions": get_total_questions_count(),
         "total_games": get_total_games(),
         "total_groups": get_total_groups(),
